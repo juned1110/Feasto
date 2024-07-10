12fe2fd272be7c10c8c64fff47c097cc00ccda00
juned1110 <junedking1110@gmail.com>
2024-07-10 16:15:00 +0530

    header UI created

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -4,7 +4,18 @@ import "./Header.css";
 const Header = () => {
   return (
     <React.Fragment>
-      <div>Header</div>
+      <div className="header">
+        <div className="header-contents">
+          <h2>Order your favourite food here</h2>
+          <p>
+            Choose from a diverse menu featuring a delectable array of dishes
+            crafted with the finest ingredients and culinary expertise. Satisfy
+            your craving and elevate your dinig experience, one delicious meal
+            at a time.
+          </p>
+          <button>View Menu</button>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Home.css";
+import Header from "../../component/Header/Header";
 
 const Home = () => {
   return (
     <React.Fragment>
-      <div>Home</div>
+      <div>
+        <Header />
+      </div>
     </React.Fragment>
   );
 };
