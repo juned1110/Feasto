12a192691ec5b5099ee0265df2efb4b1c6f7949a
juned1110 <junedking1110@gmail.com>
2024-07-10 15:39:13 +0530

    home page created

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <div>Home</div>
+    </React.Fragment>
+  );
+};
+
+export default Home;
