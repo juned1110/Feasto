0d304ec80123e293f73b5af6d287d18c7911f661
juned1110 <junedking1110@gmail.com>
2024-07-10 15:39:33 +0530

    Place Order page created

---
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlaceOrder = () => {
+  return (
+    <React.Fragment>
+      <div>PlaceOrder</div>
+    </React.Fragment>
+  );
+};
+
+export default PlaceOrder;
