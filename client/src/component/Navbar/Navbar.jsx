d1e2275788d50ac462611769d6d583d3a81ffd54
juned1110 <junedking1110@gmail.com>
2024-07-10 15:15:50 +0530

    Navbar Created

--- client/src/component/Navbar/Navbar.jsx
+++ client/src/component/Navbar/Navbar.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
   return (
     <React.Fragment>
-      <div className="Navbar">
-        <img
-          src={assets.logo}
-          alt="logo"
-          className="logo"
-          style={{ width: "5rem" }}
-        />
+      <div className="navbar">
+        <img src={assets.logo} alt="logo" className="logo" />
         <ul className="navbar-menu">
-          <li>home</li>
-          <li>menu</li>
-          <li>mobile-app</li>
-          <li>contact-us</li>
+          <li
+            onClick={() => setMenu("home")}
+            className={menu === "home" ? "active" : ""}
+          >
+            home
+          </li>
+          <li
+            onClick={() => setMenu("menu")}
+            className={menu === "menu" ? "active" : ""}
+          >
+            menu
+          </li>
+          <li
+            onClick={() => setMenu("mobile-app")}
+            className={menu === "mobile-app" ? "active" : ""}
+          >
+            mobile-app
+          </li>
+          <li
+            onClick={() => setMenu("contact-us")}
+            className={menu === "contact-us" ? "active" : ""}
+          >
+            contact-us
+          </li>
         </ul>
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
