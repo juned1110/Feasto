1dc44169ed4a31ad549869a3e14ac274c099b12a
juned1110 <junedking1110@gmail.com>
2024-07-10 15:35:36 +0530

    cart created

---
+++ client/src/pages/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = () => {
+  return (
+    <React.Fragment>
+      <div>Cart</div>
+    </React.Fragment>
+  );
+};
+
+export default Cart;
