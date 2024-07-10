f536a07466252bbf0b9443ae316fc22cc0a981e3
juned1110 <junedking1110@gmail.com>
2024-07-10 15:57:00 +0530

    header css created

---
+++ client/src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <div>Header</div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
