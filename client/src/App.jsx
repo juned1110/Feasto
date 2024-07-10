d7b8d1dcbee414c380e512e123763b3fd2d13c08
juned1110 <junedking1110@gmail.com>
2024-07-10 15:54:02 +0530

    routing given to home, cart and placeOrder component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <React.Fragment>
       <div className="app">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
       </div>
     </React.Fragment>
   );
