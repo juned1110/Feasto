8a6298fd25bfbeb3da4a176614750544fb78914e
juned1110 <junedking1110@gmail.com>
2024-07-11 14:00:24 +0530

    ExploreMenu Component UI created with active functionallity

--- client/src/component/ExploreMenu/ExploreMenu.jsx
+++ client/src/component/ExploreMenu/ExploreMenu.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <React.Fragment>
-      <div>ExploreMenu</div>
+      <div className="explore-menu" id="explore-menu">
+        <h1>Explore our menu</h1>
+        <p className="explore-menu-text">
+          Choose from a diverse menu featuring a delectable array of dishes. Our
+          mission is to satisfy your craving and elevate your dining experience,
+          one delicious meal at a time.
+        </p>
+        <div className="explore-menu-list">
+          {menu_list.map((item, index) => {
+            return (
+              <div
+                onClick={() =>
+                  setCategory((prev) =>
+                    prev === item.menu_name ? "All" : item.menu_name
+                  )
+                }
+                key={index}
+                className="explore-menu-list-item"
+              >
+                <img className={category===item.menu_name? "active": ""} src={item.menu_image} alt="" />
+                <p>{item.menu_name}</p>
+              </div>
+            );
+          })}
+        </div>
+        <hr />
+      </div>
     </React.Fragment>
   );
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../component/Header/Header";
+import ExploreMenu from "../../component/ExploreMenu/ExploreMenu";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
     <React.Fragment>
       <div>
         <Header />
+        <ExploreMenu category={category} setCategory={setCategory} />
       </div>
     </React.Fragment>
   );
