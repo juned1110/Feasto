0c089eaba969ba6b4824f93e629684fc8c600d69
juned1110 <junedking1110@gmail.com>
2024-07-11 13:21:10 +0530

    ExploreMenu Component created

---
+++ client/src/component/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ExploreMenu.css";
+
+const ExploreMenu = () => {
+  return (
+    <React.Fragment>
+      <div>ExploreMenu</div>
+    </React.Fragment>
+  );
+};
+
+export default ExploreMenu;
