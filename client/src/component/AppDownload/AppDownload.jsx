71a248a529ebcc902db98de9ab2867e7330403fe
juned1110 <junedking1110@gmail.com>
2024-07-12 17:14:43 +0530

    App Download functionallit created

--- client/src/component/AppDownload/AppDownload.jsx
+++ client/src/component/AppDownload/AppDownload.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./AppDownload.css";
+import { assets } from "../../assets/assets";
 
 const AppDownload = () => {
   return (
     <React.Fragment>
-      <div>AppDownload</div>
+      <div className="app-download" id="app-download">
+        <p>
+          For Better Experience Download <br />
+          Feasto App
+        </p>
+        <div className="app-download-platforms">
+          <img src={assets.play_store} alt="" />
+          <img src={assets.app_store} alt="" />
+        </div>
+      </div>
     </React.Fragment>
   );
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../../component/Header/Header";
 import ExploreMenu from "../../component/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../component/FoodDisplay/FoodDisplay";
+import AppDownload from "../../component/AppDownload/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -13,6 +14,7 @@ const Home = () => {
         <Header />
         <ExploreMenu category={category} setCategory={setCategory} />
         <FoodDisplay category={category} />
+        <AppDownload />
       </div>
     </React.Fragment>
   );
