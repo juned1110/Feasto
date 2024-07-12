3a4274f94c62d90b601338ff52a2fac73fa1c39b
juned1110 <junedking1110@gmail.com>
2024-07-12 17:01:00 +0530

    App Download component created

---
+++ client/src/component/AppDownload/AppDownload.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppDownload.css";
+
+const AppDownload = () => {
+  return (
+    <React.Fragment>
+      <div>AppDownload</div>
+    </React.Fragment>
+  );
+};
+
+export default AppDownload;
