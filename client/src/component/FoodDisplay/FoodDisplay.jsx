2048639f571720f17a3b5dd18bfa1547154cfcfd
juned1110 <junedking1110@gmail.com>
2024-07-12 14:42:19 +0530

    FoodDisplay Component created

---
+++ client/src/component/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FoodDisplay.css";
+
+const FoodDisplay = () => {
+  return (
+    <React.Fragment>
+      <div>FoodDisplay</div>
+    </React.Fragment>
+  );
+};
+
+export default FoodDisplay;
