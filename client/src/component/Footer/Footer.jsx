a3bf33117759ecbb694949d684f1a292dd8f4102
juned1110 <junedking1110@gmail.com>
2024-07-12 16:36:10 +0530

    Footer file created

---
+++ client/src/component/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <div>Footer</div>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
