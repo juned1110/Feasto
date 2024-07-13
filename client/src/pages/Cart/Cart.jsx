24ba15b2cfc6d1dd396e06df329889863b658d59
juned1110 <junedking1110@gmail.com>
2024-07-13 12:55:01 +0530

    add to cart grid created including functionality

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -22,14 +22,22 @@ const Cart = () => {
           {food_list.map((item, index) => {
             if (cartItems[item._id] > 0) {
               return (
-                <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
-                  <p>{item.name}</p>
-                  <p>{item.price}</p>
-                  <p>{cartItems[item._id]}</p>
-                  <p>{item.price * cartItems[item._id]}</p>
-                  <p>x</p>
-                </div>
+                <React.Fragment>
+                  <div className="cart-items-title cart-items-item">
+                    <img src={item.image} alt="" />
+                    <p>{item.name}</p>
+                    <p> ₹{item.price}</p>
+                    <p>{cartItems[item._id]}</p>
+                    <p> ₹{item.price * cartItems[item._id]}</p>
+                    <p
+                      onClick={() => removeFromCart(item._id)}
+                      className="cross"
+                    >
+                      x
+                    </p>
+                  </div>
+                  <hr />
+                </React.Fragment>
               );
             }
           })}
