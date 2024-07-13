17501eab7c174309b9f0a7e5ed8b0e2f6b4667b1
juned1110 <junedking1110@gmail.com>
2024-07-13 13:01:39 +0530

    cart checkout created

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -42,6 +42,37 @@ const Cart = () => {
             }
           })}
         </div>
+        <div className="cart-bottom">
+          <div className="cart-total">
+            <h2>Cart Totals</h2>
+            <div>
+              <div className="cart-total-details">
+                <p>Subtotal</p>
+                <p>{0}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <p>Delievery Fee</p>
+                <p>{2}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <b>Total</b>
+                <b>{0}</b>
+              </div>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+          </div>
+          <div className="cart-promocode">
+            <div>
+              <p>If you have a promo code, Enter it here</p>
+              <div className="cart-promocode-input">
+                <input type="text" placeholder="Promo code" />
+                <button>Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
