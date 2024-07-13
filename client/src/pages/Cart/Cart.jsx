7b768c8f05eea60a4a8427344e11376ab601cb64
juned1110 <junedking1110@gmail.com>
2024-07-13 12:43:14 +0530

    Add to cart functionality created

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -19,6 +19,20 @@ const Cart = () => {
           </div>
           <br />
           <hr />
+          {food_list.map((item, index) => {
+            if (cartItems[item._id] > 0) {
+              return (
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p>x</p>
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     </React.Fragment>
