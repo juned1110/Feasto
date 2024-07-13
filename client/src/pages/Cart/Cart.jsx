ed080dc5c238498a05b7c3bc494147360d130b24
juned1110 <junedking1110@gmail.com>
2024-07-13 12:34:36 +0530

    cart page created

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
+  const { food_list, cartItems, removeFromCart } = useContext(StoreContext);
+
   return (
     <React.Fragment>
-      <div>Cart</div>
+      <div className="cart">
+        <div className="cart-items">
+          <div className="cart-items-title">
+            <p>Items</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+          </div>
+          <br />
+          <hr />
+        </div>
+      </div>
     </React.Fragment>
   );
 };
