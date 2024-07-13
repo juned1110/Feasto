bc2e3a8786bf40f7290a6250cf7d2af848c36eb9
juned1110 <junedking1110@gmail.com>
2024-07-13 11:52:40 +0530

    LoginPopup component created

---
+++ client/src/component/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoginPopup.css";
+
+const LoginPopup = () => {
+  return (
+    <React.Fragment>
+      <div>LoginPopup</div>
+    </React.Fragment>
+  );
+};
+
+export default LoginPopup;
