5718fb9e5fbf3cf46f5f2bea0b3125cf144bef93
juned1110 <junedking1110@gmail.com>
2024-07-25 14:24:14 +0530

    foodController configured

---
+++ server/controllers/foodController.js
@@ -0,0 +1,25 @@
+import foodModel from "../models/foodModel.js";
+import fs from "fs";
+
+//add food item
+
+const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
+
+  const food = new foodModel({
+    name: req.body.name,
+    descrption: req.body.descrption,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { addFood };
