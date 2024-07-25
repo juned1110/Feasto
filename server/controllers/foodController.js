dc54d9d4aea7c27e8795c044204cf77b3661d3f0
juned1110 <junedking1110@gmail.com>
2024-07-25 16:57:03 +0530

    bug fix

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -2,13 +2,12 @@ import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
 //add food item
-
 const addFood = async (req, res) => {
   let image_filename = `${req.file.filename}`;
 
   const food = new foodModel({
     name: req.body.name,
-    descrption: req.body.descrption,
+    description: req.body.description,
     price: req.body.price,
     category: req.body.category,
     image: image_filename,
