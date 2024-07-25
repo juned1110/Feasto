e43efc11eb101c483c3ea7ff767e9ad6d4ef27a0
juned1110 <junedking1110@gmail.com>
2024-07-25 14:23:30 +0530

    server.js configed

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+
+//app config
+const app = express();
+const port = 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//DB connection
+connectDB();
+
+//api endpoints
+app.use("/api/food", foodRouter);
+
+app.get("/", (req, res) => {
+  res.send("server is working");
+});
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
