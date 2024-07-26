4ba4ba647a4540314c9878759a8953c88ee6590c
juned1110 <junedking1110@gmail.com>
2024-07-26 10:31:35 +0530

    Orders component created

---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Orders.css";
+
+const Orders = () => {
+  return (
+    <React.Fragment>
+      <div>Orders</div>
+    </React.Fragment>
+  );
+};
+
+export default Orders;
