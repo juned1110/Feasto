7b417890cb4566265fe3b97c8e66967617ac2d82
juned1110 <junedking1110@gmail.com>
2024-07-26 10:25:39 +0530

    Routess added to add, list, orders anbd Toastcontainer added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Navbar from "./component/Navbar/Navbar";
+import Sidebar from "./component/sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const url = "http://localhost:4000";
+
   return (
     <React.Fragment>
-      <div>App</div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+        </Routes>
+      </div>
     </React.Fragment>
   );
 };
