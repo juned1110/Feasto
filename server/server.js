7d29311e7daac990dc3a9f51f2b367ecbc16be4b
juned1110 <junedking1110@gmail.com>
2024-07-27 10:57:26 +0530

    user route created and env file configured

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 //app config
 const app = express();
@@ -17,6 +21,7 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("server is working");
