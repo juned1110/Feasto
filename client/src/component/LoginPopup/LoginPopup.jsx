24354eb3ce9d4e8991873139ddedd849d7eca85d
juned1110 <junedking1110@gmail.com>
2024-07-27 10:55:17 +0530

    login signup popup with functionality created

--- client/src/component/LoginPopup/LoginPopup.jsx
+++ client/src/component/LoginPopup/LoginPopup.jsx
@@ -1,37 +1,96 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./LoginPopup.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
 const LoginPopup = ({ setShowLogin }) => {
   const [currState, setCurrState] = useState("Login");
 
+  const { url, setToken } = useContext(StoreContext);
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onLogin = async (event) => {
+    event.preventDefault();
+    let newUrl = url;
+    if (currState === "Login") {
+      newUrl += "/api/user/login";
+    } else {
+      newUrl += "/api/user/register";
+    }
+
+    try {
+      const response = await axios.post(newUrl, data);
+
+      if (response.data.success) {
+        setToken(response.data.token);
+        localStorage.setItem("token", response.data.token);
+        setShowLogin(false);
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      alert("An error occurred. Please try again.");
+    }
+  };
+
   return (
     <React.Fragment>
       <div className="login-popup">
-        <form className="login-popup-container">
+        <form onSubmit={onLogin} className="login-popup-container">
           <div className="login-popup-title">
             <h2>{currState}</h2>
             <img
               onClick={() => setShowLogin(false)}
               src={assets.cross_icon}
-              alt=""
+              alt="close"
             />
           </div>
           <div className="login-popup-input">
-            {currState === "Login" ? (
-              <></>
-            ) : (
-              <input type="text" placeholder="Your name" required />
+            {currState === "Sign Up" && (
+              <input
+                name="name"
+                onChange={onChangeHandler}
+                value={data.name}
+                type="text"
+                placeholder="Your name"
+                required
+              />
             )}
-            <input type="email" placeholder="Your email" required />
-            <input type="password" placeholder="Password" required />
+            <input
+              name="email"
+              onChange={onChangeHandler}
+              value={data.email}
+              type="email"
+              placeholder="Your email"
+              required
+            />
+            <input
+              name="password"
+              onChange={onChangeHandler}
+              value={data.password}
+              type="password"
+              placeholder="Password"
+              required
+            />
           </div>
-          <button>
-            {currState === "Sign Up " ? "Create account" : "Login"}
+          <button type="submit">
+            {currState === "Sign Up" ? "Create account" : "Login"}
           </button>
           <div className="login-popup-condition">
             <input type="checkbox" required />
-            <p>By Continuing, I agree to the term of use & privacy policy.</p>
+            <p>By Continuing, I agree to the terms of use & privacy policy.</p>
           </div>
           {currState === "Login" ? (
             <p>
