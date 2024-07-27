b8b907a00eaf5bb1dcb22f6a35b4aff7bf5649c8
juned1110 <junedking1110@gmail.com>
2024-07-27 10:55:46 +0530

    navbar updated

--- client/src/component/Navbar/Navbar.jsx
+++ client/src/component/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { StoreContext } from "../../context/StoreContext";
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
 
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
   return (
     <React.Fragment>
@@ -46,14 +54,31 @@ const Navbar = ({ setShowLogin }) => {
           </a>
         </ul>
         <div className="navbar-right">
-          <img src={assets.search_icon} alt="" />
+          <img src={assets.search_icon} alt="search" />
           <div className="navbar-search-icon">
             <Link to="/cart">
-              <img src={assets.basket_icon} alt="" />
+              <img src={assets.basket_icon} alt="basket" />
             </Link>
             <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
           </div>
-          <button onClick={() => setShowLogin(true)}>sign in</button>
+          {!token ? (
+            <button onClick={() => setShowLogin(true)}>sign in</button>
+          ) : (
+            <div className="navbar-profile">
+              <img src={assets.profile_icon} alt="profile" />
+              <ul className="nav-profile-dropdown">
+                <li>
+                  <img src={assets.bag_icon} alt="orders" />
+                  <p>Orders</p>
+                </li>
+                <hr />
+                <li onClick={logout}>
+                  <img src={assets.logout_icon} alt="logout" />
+                  <p>Logout</p>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </React.Fragment>
