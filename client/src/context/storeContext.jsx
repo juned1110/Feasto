91f7f2568c845ddcd17b69afe539653ec4c20222
juned1110 <junedking1110@gmail.com>
2024-07-27 12:05:10 +0530

    food list fetching functionality in context

--- client/src/context/storeContext.jsx
+++ client/src/context/storeContext.jsx
@@ -1,14 +1,13 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-
-  const url = "http://localhost:4000";
-
+  const [food_list, setFoodList] = useState([]);
   const [token, setToken] = useState("");
+  const url = "http://localhost:4000";
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -33,11 +32,25 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   };
 
+  const fetchFoodList = async () => {
+    try {
+      const response = await axios.get(url + "/api/food/list");
+      setFoodList(response.data.data);
+    } catch (error) {
+      console.error("Error fetching food list:", error);
+    }
+  };
+
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
-    if (storedToken) {
-      setToken(storedToken);
+
+    async function loadData() {
+      await fetchFoodList();
+      if (storedToken) {
+        setToken(storedToken);
+      }
     }
+    loadData();
   }, []);
 
   const contextValue = {
