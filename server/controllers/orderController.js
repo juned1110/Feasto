39be7634b0cabfd5f6f7e964646f1d253e0fd145
juned1110 <junedking1110@gmail.com>
2024-08-02 18:20:41 +0530

    verifyorder and userorders added

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -64,4 +64,31 @@ const placeOrder = async (req, res) => {
   }
 };
 
-export { placeOrder };
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success == "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+//user orders for frontend
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, messaage: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders };
