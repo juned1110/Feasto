8286452d3a6e881e9828ce8e8aabb750adcd82b5
juned1110 <junedking1110@gmail.com>
2024-08-02 16:34:09 +0530

    accesing stripe key and order route created

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
-import dotenv from "dotenv";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 dotenv.config();
+console.log("Stripe API Key from .env:", process.env.STRIPESECRET_KEY);
 
 //app config
 const app = express();
@@ -24,6 +26,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("server is working");
