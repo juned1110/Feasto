9ac30f7b5b67e02e54800bfa8303bf49a01a34b0
juned1110 <junedking1110@gmail.com>
2024-08-06 14:49:38 +0530

    route added

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
 import {
+  listOrders,
   placeOrder,
+  updateStatus,
   userOrders,
   verifyOrder,
 } from "../controllers/orderController.js";
@@ -11,5 +13,7 @@ const orderRouter = express.Router();
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrders);
+orderRouter.get("/list", listOrders);
+orderRouter.post("/status", updateStatus);
 
 export default orderRouter;
