a2d350dcf53e2eb2e63f99a713efe3f0c94d7012
juned1110 <112263208+juned1110@users.noreply.github.com>
2024-08-13 13:03:28 +0530

    Update storeContext.jsx

--- client/src/context/storeContext.jsx
+++ client/src/context/storeContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://feasto-server.onrender.com";
   const [token, setToken] = useState("");
   const [foodList, setFoodList] = useState([]);
 
