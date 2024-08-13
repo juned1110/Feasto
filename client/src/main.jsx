9927e5c62b9872cb3077aa6fdce5236c6d0abb6a
juned1110 <112263208+juned1110@users.noreply.github.com>
2024-08-13 13:20:39 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./context/StoreContext.jsx";
+import StoreContext from "./context/StoreContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
