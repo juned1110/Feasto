6794a22a1fa779cfa34b3e4b1ed5b837c6fb965a
juned1110 <junedking1110@gmail.com>
2024-08-13 13:26:13 +0530

    chnages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
+//changes
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
